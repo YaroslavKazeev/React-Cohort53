31f7a8ba030545c4c17818a8cc8f6de8297b7fd8
Yaroslav <1.yaroslav.kazeev@gmail.com>
2025-09-23 16:32:57 +0200

    HTML is split into categories and products components

---
+++ week1/project/ecommerce/src/Categories.jsx
@@ -0,0 +1,10 @@
+export default function Categories() {
+  return (
+    <div class="categories">
+      <div class="categories--item">electronics</div>
+      <div class="categories--item">jewelery</div>
+      <div class="categories--item">men's clothing</div>
+      <div class="categories--item">women's clothing</div>
+    </div>
+  );
+}
